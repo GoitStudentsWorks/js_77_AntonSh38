3c4f7dbef35872e5375ee92189dc11238a4e2737
Anton Shuvalov <anshuvalov83@gmail.com>
2025-09-20 11:38:23 +0300

    add loader

---
+++ src/js/loader.js
@@ -0,0 +1,9 @@
+const loadeEl = document.querySelector('.loader');
+
+export function showLoader() {
+  loadeEl.classList.remove(is - hiden);
+}
+
+export function hideLoader() {
+  loadeEl.classList.add(is - hiden);
+}
