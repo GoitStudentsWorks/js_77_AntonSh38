035924420910cf8b90c9694bd773691e0499fb12
Anton Shuvalov <anshuvalov83@gmail.com>
2025-09-20 17:44:25 +0300

    update html

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/project_team_js2/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
