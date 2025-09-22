6a96fe33b41dfec9a848d8fa734ad5dc97079026
Dima Kravchenko <dima.kravchenko.1999@gmail.com>
2025-09-22 13:17:49 +0300

    fix photo

--- src/js/about.js
+++ src/js/about.js
@@ -8,29 +8,29 @@ import 'swiper/css/pagination';
 const data = [
   {
     id: 1,
-    imageUrl: '../img/about/mobile-photo/mobile1x-photo1.jpg',
+    imageUrl: './img/about/mobile-photo/mobile1x-photo1.jpg',
     text: 'Все почалося у 2015 році з кількох небайдужих людей та одного врятованого собаки. Сьогодні ми — один з найбільших притулків у регіоні, але наша мета незмінна: дати другий шанс тим, кого зрадили.',
   },
   {
     id: 2,
-    imageUrl: '../img/about/mobile-photo/mobile1x-photo2.jpg',
+    imageUrl: './img/about/mobile-photo/mobile1x-photo2.jpg',
     text: 'Ми рятуємо, реабілітуємо та знаходимо люблячі родини для безпритульних тварин. Наша мета — не просто дати прихисток, а й забезпечити кожному "хвостику" щасливе та повноцінне життя в новій родині.',
   },
 
   {
     id: 3,
+    imageUrl: './img/about/mobile-photo/mobile1x-photo3.jpg',
     text: `"Хатинка Лапок" — це команда професійних ветеринарів, кінологів та десятків волонтерів, які щодня вкладають свою душу та час у турботу про наших підопічних. Ми працюємо 24/7, бо їхнє життя залежить від нас.`,
-    imageUrl: '../img/about/mobile-photo/mobile1x-photo3.jpg',
   },
   {
     id: 4,
+    imageUrl: './img/about/mobile-photo/mobile1x-photo4.jpg',
     text: `Ми створили безпечний та комфортний простір. Кожна тварина отримує якісне харчування, своєчасну ветеринарну допомогу, проходить соціалізацію та гуляє на спеціально обладнаних майданчиках.`,
-    imageUrl: '../img/about/mobile-photo/mobile1x-photo4.jpg',
   },
   {
     id: 5,
     text: `Ваша допомога — безцінна. Ви можете взяти тваринку додому, стати волонтером, допомогти фінансово або інформаційно. Кожен маленький внесок наближає нас до великої мети — світу без безпритульних тварин.`,
-    imageUrl: '../img/about/mobile-photo/mobile1x-photo5.jpg',
+    imageUrl: './img/about/mobile-photo/mobile1x-photo5.jpg',
   },
 ];
 
@@ -39,11 +39,11 @@ const aboutRender = data.map(({ id, text, imageUrl }) => {
    <div class="swiper-slide about-slide">
     <picture>
       <!-- десктоп -->
-      <source media="(min-width: 1440px)" srcset="../img/about/desktop-photo/desktop1x-photo${id}.jpg 1x,
-       ../img/about/desktop-photo/desktop2x-photo${id}.jpg 2x">
+      <source media="(min-width: 1440px)" srcset="./img/about/desktop-photo/desktop1x-photo${id}.jpg 1x,
+       ./img/about/desktop-photo/desktop2x-photo${id}.jpg 2x">
       <!-- планшет -->
-      <source media="(min-width: 768px)" srcset="../img/about/tablet-photo/tablet1x-photo${id}.jpg 1x,
-      ../img/about/tablet-photo/tablet2x-photo${id}.jpg 2x">
+      <source media="(min-width: 768px)" srcset="./img/about/tablet-photo/tablet1x-photo${id}.jpg 1x,
+      ./img/about/tablet-photo/tablet2x-photo${id}.jpg 2x">
       <!-- мобілка -->
       <img src="${imageUrl}" alt="about" class="about-img">
     </picture>
