49ec967be59e68dcc5e35e7e859904080e0a1319
Anton Shuvalov <anshuvalov83@gmail.com>
2025-09-23 09:50:56 +0300

    svg

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,31 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const accordionItems = document.querySelectorAll(".accordion-item");
+document.addEventListener('DOMContentLoaded', () => {
+  const accordionItems = document.querySelectorAll('.accordion-item');
 
-  accordionItems.forEach((item) => {
-    const title = item.querySelector(".accordion-title");
-    const iconUse = item.querySelector("svg use");
+  accordionItems.forEach(item => {
+    const title = item.querySelector('.accordion-title');
+    const iconUse = item.querySelector('svg use');
     const content = item.nextElementSibling;
-    const clickableElements = [title, item.querySelector("svg")];
+    const clickableElements = [title, item.querySelector('svg')];
 
     content.style.maxHeight = null;
 
-    clickableElements.forEach((el) => {
-      el.addEventListener("click", () => {
-        const isOpen = content.style.maxHeight && content.style.maxHeight !== "0px";
+    clickableElements.forEach(el => {
+      el.addEventListener('click', () => {
+        const isOpen =
+          content.style.maxHeight && content.style.maxHeight !== '0px';
 
-        document.querySelectorAll(".accordion-content").forEach((c) => {
+        document.querySelectorAll('.accordion-content').forEach(c => {
           c.style.maxHeight = null;
         });
-        accordionItems.forEach((i) => {
-          const useEl = i.querySelector("svg use");
-          if (useEl) useEl.setAttribute("href", "./img/sprite.svg#icon-icon-add");
-          i.classList.remove("open");
+        accordionItems.forEach(i => {
+          const useEl = i.querySelector('svg use');
+          if (useEl)
+            useEl.setAttribute(
+              'href',
+              './public/icon/sprite.svg#icon-icon-add'
+            );
+          i.classList.remove('open');
         });
 
         if (!isOpen) {
-          content.style.maxHeight = content.scrollHeight + "px";
-          if (iconUse) iconUse.setAttribute("href", "./img/sprite.svg#icon-icon-close");
-          item.classList.add("open");
+          content.style.maxHeight = content.scrollHeight + 'px';
+          if (iconUse)
+            iconUse.setAttribute(
+              'href',
+              './public/icon/sprite.svg#icon-icon-close'
+            );
+          item.classList.add('open');
         }
       });
     });
